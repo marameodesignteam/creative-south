fdeacbd22d0eeabd1417474af95640054c997f1d
Ly Xuan Ngoc <xngocly.tq@gmail.com>
2021-03-24 08:54:12 +0700

    Removed wp-config.php from git

--- dev.php
+++ dev.php
@@ -3,3 +3,8 @@ U: support@marameodesign.com
 P: mara!MeO@!Des!gn
 
 Master branch: main
+
+ssh master_pyebeyzefp@45.77.51.3
+5fGCuEbz
+
+git remote add cw_dev ssh://master_pyebeyzefp@45.77.51.3/home/master/applications/utgbbxwxxu/public_html/.git
