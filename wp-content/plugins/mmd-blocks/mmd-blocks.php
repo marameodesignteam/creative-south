846302cbda7f0c748e4dcc2127c10e2c4160cbae
Ly Xuan Ngoc <xngocly.tq@gmail.com>
2021-04-12 09:54:01 +0700

    Debug

--- wp-content/plugins/mmd-blocks/mmd-blocks.php
+++ wp-content/plugins/mmd-blocks/mmd-blocks.php
@@ -7,18 +7,6 @@
 * Author: Marameo Design
 **/
 
-function block_variation_enqueue() {
-  error_log(plugin_dir_path( __FILE__ ) . 'blocks.js');
-  wp_enqueue_script(
-    'block-variation-script',
-    plugins_url( 'blocks.js', __FILE__ ),
-    array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ),
-    filemtime( plugin_dir_path( __FILE__ ) . 'blocks.js' )
-  );
-}
-add_action( 'enqueue_block_editor_assets', 'block_variation_enqueue' );
-
-
 function mmd_blocks_category($categories, $post) {
   array_unshift($categories, array(
     'slug' => 'mmd-custom-blocks',
