fe0b33edfd8c947bd6f3372eb6e09e7349581d24
Nguyen Ngoc Vinh <ngocvinh20895@gmail.com>
2021-04-19 14:57:39 +0700

    Fixed more info

--- wp-content/themes/generatepress_child/functions/footer.php
+++ wp-content/themes/generatepress_child/functions/footer.php
@@ -52,8 +52,8 @@ if (! function_exists('generate_construct_footer')) {
       <script>
           function showOnMap(place_id) {
             jQuery("#post-" + place_id + " .place_title").click();
-            // jQuery("#post-" + place_id + " .collapseTour-item").slideDown();
-            // jQuery("#post-" + place_id + " .more-info").addClass('active');
+            jQuery("#post-" + place_id + " .collapseTour-item").slideDown();
+            jQuery("#post-" + place_id + " .more-info").addClass('active');
           }
 
           function showOnMapDes(place_id){
@@ -62,9 +62,9 @@ if (! function_exists('generate_construct_footer')) {
               // jQuery(this).toggleClass('active');
               // paren.find('.collapseTour-item').slideToggle();
               // jQuery("#" + parenId + " .place_title").click();
+              jQuery("#post-" + place_id + " .place_title").click();
               jQuery("#post-" + place_id + " .more-info").toggleClass('active');
               jQuery("#post-" + place_id + " .collapseTour-item").slideToggle();
-              jQuery("#post-" + place_id + " .place_title").click();
           }
 
       </script>
