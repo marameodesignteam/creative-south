222c087e7b0eac052981b134e801088ca9452ce3
Ly Xuan Ngoc <xngocly.tq@gmail.com>
2021-04-21 11:20:13 +0700

    Multi categ

--- wp-content/themes/generatepress_child/functions.php
+++ wp-content/themes/generatepress_child/functions.php
@@ -12,15 +12,15 @@ include("functions/header.php");
 include("functions/footer.php");
 include("functions/image-sizes.php");
 
- //add menus programmatically
+//add menus programmatically
 
- function wpb_custom_new_menu()
- {
-     register_nav_menu('header-menu', __('Header Menu'));
-     register_nav_menu('footer-menu', __('Footer Menu'));
- }
+function wpb_custom_new_menu()
+{
+	register_nav_menu('header-menu', __('Header Menu'));
+	register_nav_menu('footer-menu', __('Footer Menu'));
+}
 
- add_action('init', 'wpb_custom_new_menu');
+add_action('init', 'wpb_custom_new_menu');
 
 //add options page
 // if( function_exists('acf_add_options_page') ) {
@@ -33,29 +33,29 @@ add_post_type_support( 'page', 'excerpt' );
 ///Remove posts from side menu
 //add_action('admin_menu', 'remove_default_post_type');
 function remove_default_post_type() {
-  remove_menu_page('edit.php');
+	remove_menu_page('edit.php');
 }
 
 
 
 function generateRandomString($length = 10)
 {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
+	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$charactersLength = strlen($characters);
+	$randomString = '';
+	for ($i = 0; $i < $length; $i++) {
+		$randomString .= $characters[rand(0, $charactersLength - 1)];
+	}
+	return $randomString;
 }
 
 
 function custom_style() {
-    ?>
+	?>
     <style type="text/css">
         .components-editor-notices__dismissible {
             display: none !important;
-        } 
+        }
         body.gutenberg-editor-page .editor-post-title__block, body.gutenberg-editor-page .editor-default-block-appender, body.gutenberg-editor-page .editor-block-list__block {
             max-width: none !important;
         }
@@ -64,7 +64,7 @@ function custom_style() {
         }
         /*code editor*/
         .edit-post-text-editor__body {
-            max-width: none !important;	
+            max-width: none !important;
             margin-left: 2%;
             margin-right: 2%;
         }
@@ -75,42 +75,42 @@ add_action( 'admin_footer', 'custom_style' );
 function custom_style_login() {
 	?>
     <style type="text/css">
-    body.login-action-login{
-      background: #000;
-    }
-		.login h1 a {
-			background-image: url( <?php echo get_stylesheet_directory_uri(); ?>/images/logo-header.png);
-			background-size: 100% auto;
-			height: 84px;
-			width: 239px;
-		}
-    .login #backtoblog a, .login #nav a, .login h1 a{
-      color: #fff;
-    }
-    .login #backtoblog a:hover, .login #nav a:hover, .login h1 a:hover{
-      color: #ee3897;
-    }
-    .wp-core-ui .button-primary{
-        background:#ee3897;
-        border-color: #ee3897;
-    }
-    .wp-core-ui .button-primary:hover,
-    .wp-core-ui .button-primary:focus{
-        border-color: #ee3897;
-        background: #fff;
-        color: #ee3897;
-    }
-		.wp-social-login-provider-list img {
-			max-width:100%;
-		}
-    .login form .input:focus, .login form input[type=checkbox]:focus, .login input[type=text]:focus{
-        border-color: #ee3897;
-        box-shadow: 0 0 0 1px #ee3897;
-    }
-    .login .button.wp-hide-pw .dashicons{
-        color: #ee3897;
-    }
-	</style>
+        body.login-action-login{
+            background: #000;
+        }
+        .login h1 a {
+            background-image: url( <?php echo get_stylesheet_directory_uri(); ?>/images/logo-header.png);
+            background-size: 100% auto;
+            height: 84px;
+            width: 239px;
+        }
+        .login #backtoblog a, .login #nav a, .login h1 a{
+            color: #fff;
+        }
+        .login #backtoblog a:hover, .login #nav a:hover, .login h1 a:hover{
+            color: #ee3897;
+        }
+        .wp-core-ui .button-primary{
+            background:#ee3897;
+            border-color: #ee3897;
+        }
+        .wp-core-ui .button-primary:hover,
+        .wp-core-ui .button-primary:focus{
+            border-color: #ee3897;
+            background: #fff;
+            color: #ee3897;
+        }
+        .wp-social-login-provider-list img {
+            max-width:100%;
+        }
+        .login form .input:focus, .login form input[type=checkbox]:focus, .login input[type=text]:focus{
+            border-color: #ee3897;
+            box-shadow: 0 0 0 1px #ee3897;
+        }
+        .login .button.wp-hide-pw .dashicons{
+            color: #ee3897;
+        }
+    </style>
 <?php }
 add_action( 'login_head', 'custom_style_login' );
 
@@ -118,30 +118,63 @@ add_action( 'wp_ajax_update_map_params', 'update_map_params' );
 add_action( 'wp_ajax_nopriv_update_map_params', 'update_map_params' );
 
 function update_map_params() {
-  $args = array(
-    'post_type' => 'trip_locations',
-    'posts_per_page' => -1
-  );
-  $query = new WP_Query($args);
-  if ($query->have_posts() ) {
-    while ( $query->have_posts() ) {
-      $query->the_post();
-      $post_id = get_the_ID();
-      $category_id = get_post_meta($post_id, 'category_id' );
-      update_post_meta( $post_id, '_wpgmp_metabox_marker_id', serialize( $category_id ) ) ;
-      update_post_meta( $post_id, '_wpgmp_map_id', serialize( [2] ) ) ;
-    }
-    wp_reset_postdata();
-  }
+	$args = array(
+		'post_type' => 'trip_locations',
+		'posts_per_page' => -1
+	);
+	$query = new WP_Query($args);
+	if ($query->have_posts() ) {
+		while ( $query->have_posts() ) {
+			$query->the_post();
+			$post_id = get_the_ID();
+			$category_names = get_post_meta($post_id, 'category_names' );
 
-  wp_die();
+			$categs = [
+				1 => 'Festivals/Music & Performance',
+				2 => 'Galleries & Studios',
+				3 => 'Creative Retail & Markets',
+				4 => 'Public Art/Heritage',
+				8 => 'Museums & Heritage',
+				9 => 'Heritage',
+				10 => 'Public Art',
+				11 => 'Festival',
+				12 => 'Performance',
+				13 => 'Music & Performance/Heritage',
+				14 => 'Creative Retail',
+				15 => 'Music & Performance',
+				17 => 'Music/Performance',
+				18 => 'Festivals/Music & Performance/Don\'t Miss',
+				19 => 'Heritage & Museum',
+				20 => 'Museum & Heritage',
+			];
+
+			$category_id = [];
+			if (!empty($category_names[0])) {
+
+				$names = explode(',', $category_names[0]);
+				foreach ($names as $name) {
+					foreach ($categs as $cid => $cate) {
+						if ($cate == $name) $category_id[] = $cid;
+					}
+				}
+
+				error_log(print_r($category_id, 1));
+				update_post_meta( $post_id, '_wpgmp_metabox_marker_id', serialize($category_id) ) ;
+
+			}
+			update_post_meta( $post_id, '_wpgmp_map_id', serialize( [2] ) ) ;
+		}
+		wp_reset_postdata();
+	}
+
+	wp_die();
 }
 
 add_filter( 'body_class', 'itinerary_class_to_body' );
 function itinerary_class_to_body( $classes ) {
-    if (!empty($_GET['itinerary'])) {
-	    $classes[] = 'share-itinerary';
-    }
+	if (!empty($_GET['itinerary'])) {
+		$classes[] = 'share-itinerary';
+	}
 
 	return $classes;
 }
