c74ec704a9ae1cf23566def05118dff80d637b25
Nguyen Ngoc Vinh <ngocvinh20895@gmail.com>
2021-04-21 16:24:45 +0700

    Visit page

--- wp-content/themes/generatepress_child/functions/image-sizes.php
+++ wp-content/themes/generatepress_child/functions/image-sizes.php
@@ -2,3 +2,4 @@
 add_image_size("featured-fullwidth", 1920, 754, true);
 add_image_size("featured-halfwidth", 1200, 754, true);
 add_image_size("card-tour", 400, 255, true);
+add_image_size("carousel-size", 1920, 800, true);
