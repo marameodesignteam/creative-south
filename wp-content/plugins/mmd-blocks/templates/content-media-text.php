8b43536938a13dcae30a8699ac77c91e5514b3aa
Nguyen Ngoc Vinh <ngocvinh20895@gmail.com>
2021-04-22 10:33:41 +0700

    Added content media text blocks

---
+++ wp-content/plugins/mmd-blocks/templates/content-media-text.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Block Name:  Primary Content Block
+ *
+ * This is the template that displays the Primary Content block.
+ */
+
+$content = get_field( 'content' );
+$sidebar_images = get_field('sidebar_images');
+$col_class_content = ($sidebar_images && count($sidebar_images > 0)) ? 'col-md-7' : 'col-12';
+$col_class_sidebar = ($sidebar_images && count($sidebar_images > 0)) ? 'col-md-5' : 'col-12';
+
+if( !empty($block['anchor']) ) {
+    $id = $block['anchor'];
+}else{
+    $id = 'primary-content-' . $block['id'];
+}
+?>
+<?php if($content) : ?>
+<div id="<?php echo $id; ?>" class="media-text-blocks primary-content-blocks custom-blocks full-width background-black">
+    <div class="in_custom-block">
+       <div class="container">
+            <div class="primary-content">
+                <div class="row">
+                    <div class="<?php echo $col_class_content;?>">
+                        <?php echo $content; ?>
+                    </div>
+                    <?php if($sidebar_images && count($sidebar_images > 0)) : ?>
+                    <div class="<?php echo $col_class_sidebar;?>">
+                        <?php foreach($sidebar_images as $item) : 
+                            $imageArr = $item["image"];
+                            $image = wp_get_attachment_image( $imageArr, 'large' );   
+                        ?>
+                            <div class="sidebar-image">
+                                <?php echo $image;?>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+       </div>
+    </div>
+</div>
+<?php endif; ?>
