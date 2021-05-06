8201bf752bea53c23665669ebc757dba9b76bdd1
Nguyen Ngoc Vinh <ngocvinh20895@gmail.com>
2021-05-06 11:10:47 +0700

    Fixed

--- wp-content/themes/generatepress_child/functions/footer.php
+++ wp-content/themes/generatepress_child/functions/footer.php
@@ -16,8 +16,6 @@ if (! function_exists('generate_construct_footer')) {
       //   $type = $item->post_mime_type;
       //   //guid
       // endforeach;
-      $hide_footer = get_field('hide_footer');
-      if(!$hide_footer) :
       ?>
     <footer class="site-info" <?php generate_do_microdata('footer'); ?>>
     
@@ -145,7 +143,8 @@ if (! function_exists('generate_construct_footer')) {
 
       </script>
     </footer><!-- .site-info -->
-    <?php endif; if(is_front_page()) : ?>
+    <?php 
+    if(is_front_page()) : ?>
     <div class="modal fade modalcustom" id="welcomeModal" tabindex="-1" role="dialog" aria-hidden="true">
     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <!-- <i class="fas fa-times" aria-hidden="true"></i> -->
