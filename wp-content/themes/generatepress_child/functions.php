1a431b02136d7488cef6e67801b2ba65f0ff5335
Nguyen Ngoc Vinh <ngocvinh20895@gmail.com>
2021-05-21 14:29:41 +0700

    Function for role editor

--- wp-content/themes/generatepress_child/functions.php
+++ wp-content/themes/generatepress_child/functions.php
@@ -44,6 +44,45 @@ add_filter('comments_open', 'df_disable_comments_status', 20, 2);
 add_filter('pings_open', 'df_disable_comments_status', 20, 2);
 
 
+// Allow editors to see access the Menus page under Appearance but hide other options
+// Note that users who know the correct path to the hidden options can still access them
+function hide_menu() {
+    $user = wp_get_current_user();
+   
+   // Check if the current user is an Editor
+   if ( in_array( 'editor', (array) $user->roles ) ) {
+       
+       // They’re an editor, so grant the edit_theme_options capability if they don’t have it
+       if ( ! current_user_can( 'edit_theme_options' ) ) {
+           $role_object = get_role( 'editor' );
+           $role_object->add_cap( 'edit_theme_options' );
+       }
+       
+       // Hide the Themes page
+       remove_submenu_page( 'themes.php', 'themes.php' );
+       // Hide the Widgets page
+       remove_submenu_page( 'themes.php', 'widgets.php' );
+       // Hide the Customize page
+       remove_submenu_page( 'themes.php', 'customize.php' );
+       // Remove Customize from the Appearance submenu
+       global $submenu;
+       unset($submenu['themes.php'][6]);
+   }
+}
+add_action('admin_menu', 'hide_menu', 10);
+
+	/* Script Admin */
+function my_script(){ 
+    $user = wp_get_current_user();
+    if ( in_array( 'editor', (array) $user->roles ) ) {?>
+    <style type="text/css">#dashboard_primary,#icl_dashboard_widget,
+        #adminmenu li.menu-top#menu-appearance .wp-submenu li:last-child{
+            display: none;                
+        }
+    </style><?php
+}}
+ add_action( 'admin_footer', 'my_script' );
+
 function generateRandomString($length = 10)
 {
 	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
